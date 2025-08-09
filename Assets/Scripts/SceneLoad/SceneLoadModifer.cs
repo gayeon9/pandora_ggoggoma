08655a6a58ff59b3cafb81aa9901af8ae68cee4c
kimmeum <scarleteunchae@gmail.com>
2025-08-09 14:48:04 +0900

    9차 커밋, 카메라 문제 해결

--- Assets/Scripts/SceneLoad/SceneLoadModifer.cs
+++ Assets/Scripts/SceneLoad/SceneLoadModifer.cs
@@ -1,25 +1,62 @@
 
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Unity.Cinemachine;
+using System.Collections;
+using SuperEasyRPG;
 
 public class SceneLoadModifer : MonoBehaviour
 {
     void OnEnable()
     {
-        SceneManager.sceneLoaded += OnSceneLoaded;
+        SceneManager.sceneLoaded += OnSceneLoadedinventory;
+        SceneManager.sceneLoaded += OnSceneLoadedCamrea;
+
     }
 
     void OnDisable()
     {
-        SceneManager.sceneLoaded -= OnSceneLoaded;
+        SceneManager.sceneLoaded -= OnSceneLoadedinventory;
+        SceneManager.sceneLoaded -= OnSceneLoadedCamrea;
+
     }
 
-    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    void OnSceneLoadedinventory(Scene scene, LoadSceneMode mode)
     {
         if (Inventory.instance != null)
         {
             Inventory.instance.onChangeItem?.Invoke();
         }
     }
-}
+
+
+    private void OnSceneLoadedCamrea(Scene scene, LoadSceneMode mode)
+    {
+        StartCoroutine(SetCameraTargetNextFrame());
+    }
+
+
+    private IEnumerator SetCameraTargetNextFrame()
+    {
+        // 한 프레임 대기 → 씬 오브젝트 모두 로드될 때까지
+        yield return null;
+
+        GameObject player = GameObject.FindWithTag("Player");
+        if (player == null)
+        {
+            Debug.LogError("Player not found in scene!");
+            yield break;
+        }
+
+        var vcams = FindObjectsByType<CinemachineCamera>(FindObjectsSortMode.None);
+        foreach (var vcam in vcams)
+        {
+            vcam.Follow = player.transform;
+        }
+    
+
+    }
+
+
+    }
 
